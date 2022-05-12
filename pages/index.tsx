049209374ed2f0e6021a8ec1f0fb7c6f3117a47b
Summer <47139205+gigabet@users.noreply.github.com>
2022-05-12 09:52:10 +0300

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,32 @@
+import { Box, Container, Typography } from '@mui/material'
+import type { NextPage } from 'next'
+import Copyright from '../components/Copyright'
+import Link from '../components/Link'
+import ProTip from '../components/ProTip'
+
+const Home: NextPage = () => {
+  return (
+    <Container maxWidth='lg'>
+      <Box
+        sx={{
+          my: 4,
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Typography variant='h4' component='h1' gutterBottom>
+          MUI v5 + Next.js with TypeScript example
+        </Typography>
+        <Link href='/about' color='secondary'>
+          Go to the about page
+        </Link>
+        <ProTip />
+        <Copyright />
+      </Box>
+    </Container>
+  )
+}
+
+export default Home
